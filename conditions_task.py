86c37fcafa238379bb84f67f0a6fdcdda892cdae
Manuel Nasif <manuel126519@gmail.com>
2019-08-12 20:00:54 +0100

    conditions_task added

---
+++ conditions_task.py
@@ -0,0 +1,17 @@
+first_number = input("Enter the first number:  ")
+second_number = input("Enter the second number:  ")
+operation = input("Choose the opertaion (+, -, /, *):")
+
+if first_number.isdigit() and second_number.isdigit():
+	if operation == "+":
+		print ("The answer is " + str( int(first_number) + int(second_number) ) )
+	elif operation == "-":
+		print ("The answer is " + str( int(first_number) - int(second_number) ) )
+	elif operation == "/":
+		print ("The answer is " + str( int(first_number) / int(second_number) ) )
+	elif operation == "*":
+		print ("The answer is " + str( int(first_number) * int(second_number) ) )
+	else:
+		print ('operation "{}" is not valid'.format(operation))
+else:
+	print ("the numbers were invalid")
